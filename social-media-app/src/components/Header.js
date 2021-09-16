8cccb7ff891f5dc01458a2ba6f82cb96beb5a46c
ae2u16 <ae2u16@soton.ac.uk>
2021-09-16 09:37:04 +0100

    Working on /home

--- social-media-app/src/components/Header.js
+++ social-media-app/src/components/Header.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Header = (props) => {
     return (
@@ -6,9 +6,76 @@ const Header = (props) => {
             <Content>
                 <Logo>
                     <a href="/home">
-                        <img src="/imaghes/icon.svg" alt=''/>
+                        <img src="/assets/icon.svg" alt='' style={{width: '40px'}}/>
                     </a>
                 </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search"/>
+                    </div>
+                    <SearchIcon>
+                        <img src="/assets/search.svg" alt=''style={{width: '17px'}}/>
+                    </SearchIcon>
+                </Search>
+                <Nav>
+                    <NavListWrap>
+                        <NavList className='active'>
+                            <a>
+                                <img src="/assets/home.svg" alt='' style={{width: '25px'}}/>
+                                <span>Home</span>
+                            </a>
+                        </NavList>
+                        
+                        <NavList>
+                            <a>
+                                <img src="/assets/nav-network.svg" alt='' style={{width: '25px'}}/>
+                                <span>Friends</span>
+                            </a>
+                        </NavList>
+                        
+                        <NavList>
+                            <a>
+                                <img src="/assets/nav-jobs.svg" alt='' style={{width: '25px'}}/>
+                                <span>Opportunities</span>
+                            </a>
+                        </NavList>
+                        
+                        <NavList>
+                            <a>
+                                <img src="/assets/nav-messaging.svg" alt='' style={{width: '25px'}}/>
+                                <span>Messaging</span>
+                            </a>
+                        </NavList>
+                        
+                        <NavList>
+                            <a>
+                                <img src="/assets/nav-notifications.svg" alt='' style={{width: '25px'}}/>
+                                <span>Notifications</span>
+                            </a>
+                        </NavList>
+                        
+                        <User>
+                            <a>
+                                <img src="/assets/user.svg" alt=''/>
+                                <span>Me</span>
+                                <img src="/assets/down-icon.svg" alt=''/>
+                            </a>
+                            
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
+                        </User>
+                        
+                        <Work>
+                            <a>
+                                <img src="/assets/nav-work.svg" alt=''/>
+                                <span>Work
+                                <img src="/assets/down-icon.svg" alt=''/>
+                                </span>
+                            </a>
+                        </Work>
+                    </NavListWrap>
+                </Nav>
             </Content>
         </Container>
     
@@ -35,6 +102,163 @@ const Content = styled.div`
     max-width: 1128px;
 `;
 
-const Logo = styled.div``;
+const Logo = styled.span`
+margin-right: 8px;
+font-size: 0px;
+`;
+
+const Search = styled.div`
+    opacity: 1;
+    flex-grow: 1;
+    position: relative;
+    & > div {
+        max-width: 280px;
+        input {
+            border: none;
+            box-shadow: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            color: rgba(0, 0, 0 ,0.9);
+            width: 218px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+            font-weight: 400;
+            font-size: 14px;
+            height: 34px;
+            border-color: #dce6f1;
+            vertical-align: text-top;
+        }
+    }
+`;
+
+const SearchIcon = styled.div`
+    width: 40px;
+    position: absolute;
+    z-index: 1;
+    top: 10px;
+    left: 2px;
+    border-radius: 0 2px 2px 0;
+    margin: 0;
+    pointer-events: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Nav = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media (max-width: 768px){
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        background: white;
+        width: 100%;
+    }
+`;
+
+const NavListWrap = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none; 
+    
+    .active {
+        span:after{
+            content: "";
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #fff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            transition: transform .2s ease-in-out;
+            width: 100%;
+            border-color: rgba(0, 0, 0, 0.9);
+        }
+    }
+`;
+
+const NavList = styled.div`
+    display: flex;
+    align-items: center;
+    a {
+     align-items: center;
+     background: transparent;
+     display: flex;
+     flex-direction: column;
+     font-size: 12px;
+     font-weight: 400;
+     justify-content: center;
+     line-height: 1.5;
+     min-height: 42px;
+     min-width: 80px;
+     position: relative;
+     text-decoration: none;
+     
+     span {
+         color: rgba(0, 0, 0, 0.6);
+         display: flex;
+         align-items: center;
+     }
+     
+     @media (max-width: 768px){
+         min-width: 70px;
+     }
+     
+    }
+    
+    &:hover, &:active {
+        a{
+            span{
+                color: rgba(0,0,0, 0.9)
+            }
+        }
+    }
+`;
+
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+`;
+
+const User = styled(NavList)`
+    a > svg {
+        width: 24px;
+        border-radius: 50%;
+        
+    }
+    
+    a > img{
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+    
+    span{
+        display: flex;
+        align-items: center;
+        
+    }
+    
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+`;
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
+`;
+
 
 export default Header;
